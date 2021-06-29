d5921050a8ec0bdc955d2e376a9c4be2037180be
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-06-29 11:51:31 -0300

    Atualização de dependências

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Strings {
-    static formatCode(id: number, length: number): string;
-    static getFirstName(name: string): string;
-    static firstCharToUpper(str: string): string;
-    static toUrl(value: string): string;
-    static createFindRegex(value: string): RegExp;
-}
-export default Strings;
